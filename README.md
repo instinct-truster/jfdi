25fa19503d4a9c9c507b0dfa1b615b29b34759d6
Lance Turbes <instinct_truster@protonmail.com>
2023-08-08 16:46:30 -0700

    feat: add printing of goals to list

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # JFDI
 
 Inspirational Goal-Setter
+
+const element = document.createElement("div");
+
+document.appendChild();
+
+element.setAttribute();
+
+element.className = "";
+
+element.appendChild();
+
+element.remove();
+
+element.addEventListener("eventnamehere", (event) => {
+// do stuff here
+});
+
+element.textContent = ""
--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 const freshIdea = document.querySelector("#fresh");
 
 const freshIdeas = [
-  "...a brisk morning walk as the birds begin to stir. ",
-  "...lacing up your favorite shoes and heading out for a run.",
-  "...keeping in touch with the Bible's wisdom",
+  "...Take a brisk morning walk as the birds begin to stir. ",
+  "...Lace up your favorite shoes and head out for a run at dusk.",
+  "...Enjoy a sample from the Bible's wisdom and apply it to your day.",
+  "...Surprise a loved one by mailing them a hand written letter.",
+  "...Widdle a small piece of wood into a figurine heirloom.",
+  "...Brighten a stranger's mood with a smile and a nod.",
 ];
-
 function newFreshIdea() {
   let newRandomIdea = Math.floor(Math.random() * freshIdeas.length);
   document.getElementById("fresh").placeholder = freshIdeas[newRandomIdea];
 }
 newFreshIdea();
+
+const submitButton = document.querySelector("#submit-button");
+
+function handleSubmit() {
+  const goalDiv = document.querySelector("#goal-list");
+  const newGoal = document.createElement("p");
+  if (freshIdea.value) {
+    newGoal.textContent = freshIdea.value;
+  }
+  freshIdea.value = "";
+  freshIdea.placeholder = "";
+  goalDiv.appendChild(newGoal);
+}
+
+submitButton.addEventListener("click", handleSubmit);
+freshIdea.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    handleSubmit();
+  }
+});
