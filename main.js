65a10838f6265a8a43dccc01bd5ce2dcf6f88e0c
Lance Turbes <instinct_truster@protonmail.com>
2023-08-10 15:02:32 -0700

    Added some styling and color

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const freshIdeas = [
   "...Lace up your favorite shoes and head out for a run at dusk.",
   "...Enjoy a sample from the Bible's wisdom and apply it to your day.",
   "...Surprise a loved one by mailing them a hand written letter.",
-  "...Widdle a small piece of wood into a figurine heirloom.",
+  "...Whittle a small piece of wood into a figurine heirloom.",
   "...Brighten a stranger's mood with a smile and a nod.",
 ];
 function newFreshIdea() {
@@ -14,7 +14,7 @@ function newFreshIdea() {
 }
 newFreshIdea();
 
-const submitButton = document.querySelector("#submit-button");
+const submitButton = document.querySelector("#goal-submit-button");
 
 function handleSubmit() {
   const goalDiv = document.querySelector("#goal-list");
